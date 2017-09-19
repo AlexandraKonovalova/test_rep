d676107008fb764677ba2dc493fd325246282d2a
Студент НИУ ВШЭ <student@basm-401-std05.staff.corp.local>
2017-09-19 11:53:58 +0300

    Практикум на семинаре

---
+++ четность.py
@@ -0,0 +1,8 @@
+number = int(input('Введите число: '))
+if number == 0:
+    print('Ноль (все считают по-разному)')
+elif number % 2 == 0:    
+    print('Число четное')
+else:
+    print('Число нечетное')
+    
