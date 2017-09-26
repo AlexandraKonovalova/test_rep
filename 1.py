8a64f834d0807ef7d38490855f71513099a9af4f
AlexandraKonovalova <sashonet23.00@mail.ru>
2017-09-27 00:41:15 +0300

    new

---
+++ 1.py
@@ -0,0 +1,36 @@
+s = "I love linguistics!"
+#print(s[2])
+##i = 2
+##print(s[i])
+
+i = 0
+
+s1 = ""
+
+##if "ov" in s:
+##    print('ov')
+##elif "o" in s:
+##    print('o')
+##for c in s:
+##    if c == "o":
+##        continue
+##    print(c)
+##    i += 1
+##    if c == "i":
+##        break
+##    s1 += c
+####    print(s)
+##print(s1)
+
+##l = len(s)
+##i = 0
+##
+##while i < l:
+##    print(s[i])
+##    i += 1
+
+##for i,c in enumerate(s):
+##    if i == 5:
+##        print(c)
+for i in range(len(s)):
+    print(s[i])
