6bbf8b24c815deb44d17a274a9cbbdb0ccd3c171
AlexandraKonovalova <31881606+AlexandraKonovalova@users.noreply.github.com>
2017-12-04 18:57:17 +0300

    Работа на семинаре

--- writing.py
+++ writing.py
@@ -1,15 +1,15 @@
-##n_words = 0
-with open("text1.txt", 'r', encoding="utf-8") as f:
+n_line = 0
+with open("text.txt", 'r', encoding="utf-8") as f:
     with open("table.txt", 'w', encoding="utf-8") as t:
         for line in f:
             s = line.split()
             if len(s) > 0:
-                t1 = len(s)
-                t2 = s[0]
-##                data = 
-                print(t1, '\t', t2)
-##                t.write(data)
+                t1 = str(len(s))
+                t2 = str(s[0])
+                n_line += 1
             else:
-                continue;
+                continue
+            t.write(str(n_line) + '\t' + t1 + '\t' + t2 + '\n')
+
 
     
