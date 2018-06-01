f73239f2216904d292197a73362fb3ffe684dcca
AlexandraKonovalova <31881606+AlexandraKonovalova@users.noreply.github.com>
2018-06-01 23:39:48 +0300

    Домашняя работа №14, вариант 3

--- hw14/hw14.py
+++ hw14/hw14.py
@@ -9,13 +9,22 @@ def read(filename):
     return text
 
 def sent(text):
-##    sent = re.split(r'[.!?] [А-Я]|[A-Z]', text)
-    sent = [i.lower().strip(SYMBS) for i in re.split(r'[.!?]= [А-Я]|[A-Z]', text)]
-    print(sent)
+    pattern = re.compile(r'[.|!|?|...|…|\n]') 
+    sent = {i.lower().strip(): i.lower().split() for i in pattern.split(text) if i}
+    return sent
+
+
+def words(d):
+    for v in d.values():
+        for i in v:
+            i = i.strip(SYMBS)
+            print('{}_{}'.format(i, str(len(i))))
+
+    
 
 def main():
-    return sent(read("text.txt"))
-
+    return words(sent(read("text.txt")))
+    
 
 if __name__ == "__main__":
     print(main())
